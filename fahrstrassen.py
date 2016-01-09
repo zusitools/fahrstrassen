ea434c95b55aae25b6c4fa38bdd24c854cb5f937
Johannes <zusitools@web.de>
2016-01-10 00:53:22 +0100

    Signalbilder-Heuristik

--- fahrstrassen.py
+++ fahrstrassen.py
@@ -9,6 +9,8 @@ from termcolor import colored
 import logging
 logging.basicConfig(level = logging.DEBUG)
 
+all_ones = 2**64 - 1
+
 class RefPunkt(object):
     def __init__(self, modul, refnr, element, richtung):
         self.modul = modul
@@ -87,6 +89,61 @@ def get_modul_aus_dateiknoten(knoten):
         return normalize_zusi_relpath(datei.attrib.get("Dateiname", dieses_modul))
     return dieses_modul
 
+# -----
+
+# Signal-LS3 -> [Animationsname]
+animationen = dict()
+
+def get_animationen(signal_ls3_relpath):
+    signal_ls3_relpath = normalize_zusi_relpath(signal_ls3_relpath)
+    if signal_ls3_relpath not in animationen:
+        tree = ET.parse(os.path.join(os.environ['ZUSI3_DATAPATH'], signal_ls3_relpath.replace('\\', os.sep)))
+        animationen[signal_ls3_relpath] = [n.attrib.get("AniBeschreibung", "?") for n in tree.findall("./Landschaft/Animation")]
+    return animationen[signal_ls3_relpath]
+
+def get_signalbild(signal, signalbild_id):
+    idx = 0
+    result = []
+    for sigframe in signal.findall("./SignalFrame/Datei"):
+        animationen = get_animationen(sigframe.attrib["Dateiname"])
+        if len(animationen) == 0:
+            idx += 1
+        else:
+            for ani_name in animationen:
+                if signalbild_id & (1 << idx) != 0:
+                    result.append(ani_name)
+                idx += 1
+
+    return "?" if len(result) == 0 else " + ".join(result)
+
+def get_signalbild_fuer_spalte(signal, spalte):
+    signalbild_id = all_ones
+    zeilen = signal.findall("./HsigBegriff")
+    anz_spalten = len(signal.findall("./VsigBegriff"))
+    matrix = signal.findall("./MatrixEintrag")
+
+    for idx, zeile in enumerate(zeilen):
+        # Betrachte nur Zeilen fuer Zugfahrten mit Geschwindigkeit > 0,
+        # sonst kann im H/V-System das Signalbild nicht bestimmt werden
+        # (bei Hp0 ist Vorsignal dunkel)
+        if float(zeile.attrib.get("HsigGeschw", 0.0)) != 0.0 and int(zeile.attrib.get("FahrstrTyp", 0)) & 4 != 0:
+            signalbild_id &= int(matrix[idx * anz_spalten + spalte].attrib.get("Signalbild", 0))
+
+    return get_signalbild(signal, signalbild_id)
+
+def get_signalbild_fuer_zeile(signal, zeile, ersatzsignal):
+    if ersatzsignal:
+        return "?"
+
+    signalbild_id = all_ones
+    anz_spalten = len(signal.findall("./VsigBegriff"))
+    matrix = signal.findall("./MatrixEintrag")
+
+    for i in range(0, anz_spalten):
+        signalbild_id &= int(matrix[zeile * anz_spalten + i].attrib.get("Signalbild", 0))
+
+    return get_signalbild(signal, signalbild_id)
+
 for f in fahrstrassen[dieses_modul]:
     print("\nFahrstrasse {} {}".format(f.attrib.get("FahrstrTyp", "?"), colored(f.attrib.get("FahrstrName", "?"), 'grey', attrs=['bold'])))
 
@@ -101,13 +158,14 @@ for f in fahrstrassen[dieses_modul]:
         if ersatzsignal or hsig_geschw != 0:
             # == 0 ohne Ersatzsignal koennen z.B. Flachkreuzungen sein
             min_geschw = geschw_min(min_geschw, hsig_geschw)
-        print(" - Hauptsignal {} {} an {} auf {} {} ({})".format(
+        print(" - Hauptsignal {} {} an {} auf {} {} ({}) {}".format(
             colored(signal.attrib.get("NameBetriebsstelle", "?"), 'blue'),
             colored(signal.attrib.get("Signalname", "?"), 'blue', attrs=['bold']),
             rp,
             ("Zeile" if not ersatzsignal else (colored("Ersatzsignal", 'grey', attrs=['underline']) + 'zeile')),
             zeile,
             colored(str_geschw(hsig_geschw), 'red', attrs=['bold']),
+            get_signalbild_fuer_zeile(signal, zeile, ersatzsignal),
         ))
 
         ksig = signal.find("./KoppelSignal")
@@ -115,13 +173,14 @@ for f in fahrstrassen[dieses_modul]:
         while ksig is not None:
             rp = get_refpunkt(get_modul_aus_dateiknoten(ksig), int(ksig.attrib.get("ReferenzNr", 0)))
             koppelsignal = rp.element.find("./Info" + rp.richtung + "Richtung/Signal")
-            print("{} - Koppelsignal {} {} an {} auf Zeile {} ({})".format(
+            print("{} - Koppelsignal {} {} an {} auf Zeile {} ({}) {}".format(
                 " " * indent,
                 colored(koppelsignal.attrib.get("NameBetriebsstelle", "?"), 'blue'),
                 colored(koppelsignal.attrib.get("Signalname", "?"), 'blue', attrs=['bold']),
                 rp,
                 zeile,
                 colored(str_geschw(float(koppelsignal.findall("./HsigBegriff")[zeile].attrib.get("HsigGeschw", 0.0))), 'red', attrs=['bold']),
+                get_signalbild_fuer_zeile(koppelsignal, zeile, ersatzsignal),
             ))
             indent += 2
             ksig = koppelsignal.find("./KoppelSignal")
@@ -131,11 +190,12 @@ for f in fahrstrassen[dieses_modul]:
         signal = rp.element.find("./Info" + rp.richtung + "Richtung/Signal")
         spalte = int(sig.attrib.get("FahrstrSignalSpalte", 0))
         vsig_geschw = float(signal.findall("./VsigBegriff")[spalte].attrib.get("VsigGeschw", 0.0))
-        print(" - Vorsignal {} {} an {} auf Spalte {} ({}) {}".format(
+        print(" - Vorsignal {} {} an {} auf Spalte {} ({}) {} {}".format(
             colored(signal.attrib.get("NameBetriebsstelle", "?"), 'cyan'),
             colored(signal.attrib.get("Signalname", "?"), 'cyan', attrs=['bold']),
             rp,
             spalte,
             colored(str_geschw(vsig_geschw), 'green', attrs=['bold']),
+            get_signalbild_fuer_spalte(signal, spalte),
             colored("!!!!", 'red', attrs=['bold']) if geschw_kleiner(min_geschw, vsig_geschw) else "",
         ))
