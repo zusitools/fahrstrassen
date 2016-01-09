13b263a69675487123dbcb22929eca8dcd12d955
Johannes <zusitools@web.de>
2016-01-09 23:54:33 +0100

    Moduluebergreifende Fahrstrassenelemente

--- fahrstrassen.py
+++ fahrstrassen.py
@@ -3,9 +3,26 @@
 import xml.etree.ElementTree as ET
 import sys
 import os
+from collections import namedtuple
 from termcolor import colored
 
-tree = ET.parse(sys.argv[1])
+import logging
+logging.basicConfig(level = logging.DEBUG)
+
+class RefPunkt(object):
+    def __init__(self, modul, refnr, element, richtung):
+        self.modul = modul
+        self.refnr = refnr
+        self.element = element
+        self.richtung = richtung
+
+    def __repr__(self):
+        global dieses_modul
+        return "Element {}{}{}".format(
+            self.element.attrib.get("Nr", "0"),
+            'n' if self.richtung == "Norm" else 'g',
+            "" if self.modul == dieses_modul else "[{}]".format(os.path.basename(self.modul.replace('\\', os.sep))),
+        )
 
 str_geschw = lambda v : "oo<{:.0f}>".format(v) if v < 0 else "{:.0f}".format(v * 3.6)
 
@@ -31,78 +48,78 @@ def get_zusi_relpath(realpath):
         realpath = os.path.abspath(realpath)
     return normalize_zusi_relpath(os.path.relpath(realpath, os.environ['ZUSI3_DATAPATH']))
 
-str_dateiname = get_zusi_relpath(sys.argv[1])
+# Modul -> (Referenznummer -> (<StrElement>-Knoten, {"Norm", "Gegen"}))
+referenzpunkte = dict()
 
-streckenelemente = dict(
+# Modul -> [<Fahrstrasse>-Knoten]
+fahrstrassen = dict()
+
+def lade_modul(zusi_relpath):
+    tree = ET.parse(os.path.join(os.environ['ZUSI3_DATAPATH'], zusi_relpath.replace('\\', os.sep)))
+    # Elementnummer -> <StrElement>-Knoten
+    streckenelemente = dict(
         (int(s.attrib.get("Nr", 0)), s)
         for s in tree.findall("./Strecke/StrElement")
     )
-
-referenzpunkte = dict(
+    referenzpunkte[zusi_relpath] = dict(
         (int(r.attrib.get("ReferenzNr", 0)), (streckenelemente[int(r.attrib.get("StrElement", 0))], "Norm" if int(r.attrib.get("StrNorm", 0)) == 1 else "Gegen"))
         for r in tree.findall("./Strecke/ReferenzElemente")
         if int(r.attrib.get("StrElement", 0)) in streckenelemente
     )
+    fahrstrassen[zusi_relpath] = tree.findall("./Strecke/Fahrstrasse")
 
-for f in tree.findall("./Strecke/Fahrstrasse"):
+dieses_modul = get_zusi_relpath(sys.argv[1])
+logging.debug("Dieses Modul: {} -> {}".format(sys.argv[1], dieses_modul))
+
+lade_modul(dieses_modul)
+logging.debug("{} Referenzpunkt(e), {} Fahrstrasse(n)".format(len(referenzpunkte[dieses_modul]), len(fahrstrassen[dieses_modul])))
+
+def get_refpunkt(modul, nummer):
+    if modul not in referenzpunkte:
+        lade_modul(modul)
+    (element, richtung) = referenzpunkte[modul][nummer]
+    return RefPunkt(modul, nummer, element, richtung)
+
+# Sucht Knoten ./Datei und liefert Modul zurueck (leerer String oder nicht vorhandener Knoten = dieses Modul)
+def get_modul_aus_dateiknoten(knoten):
+    datei = sig.find("./Datei")
+    if datei is not None:
+        return normalize_zusi_relpath(datei.attrib.get("Dateiname", dieses_modul))
+    return dieses_modul
+
+for f in fahrstrassen[dieses_modul]:
     print("\nFahrstrasse {} {}".format(f.attrib.get("FahrstrTyp", "?"), colored(f.attrib.get("FahrstrName", "?"), 'grey', attrs=['bold'])))
 
     min_geschw = -1
 
     for sig in f.findall("./FahrstrSignal"):
-        datei = sig.find("./Datei")
-        if datei is not None:
-            dateiname = datei.attrib.get("Dateiname", "")
-            if dateiname != "" and dateiname.upper() != str_dateiname:
-                print(colored(" - Hauptsignal in Modul {}".format(dateiname), 'grey'))
-                continue
-        (element, richtung) = referenzpunkte[int(sig.attrib.get("Ref", 0))]
-        signal = element.find("./Info" + richtung + "Richtung/Signal")
+        rp = get_refpunkt(get_modul_aus_dateiknoten(sig), int(sig.attrib.get("Ref", 0)))
+        signal = rp.element.find("./Info" + rp.richtung + "Richtung/Signal")
         ersatzsignal = int(sig.attrib.get("FahrstrSignalErsatzsignal", 0)) == 1
         zeile = int(sig.attrib.get("FahrstrSignalZeile", 0))
         hsig_geschw = float(signal.findall("./HsigBegriff")[zeile].attrib.get("HsigGeschw", 0.0)) if not ersatzsignal else 0.0
         if ersatzsignal or hsig_geschw != 0:
             # == 0 ohne Ersatzsignal koennen z.B. Flachkreuzungen sein
             min_geschw = geschw_min(min_geschw, hsig_geschw)
-        print(" - Hauptsignal {} {} an Element {} {} auf {} {} ({})".format(
+        print(" - Hauptsignal {} {} an {} auf {} {} ({})".format(
             colored(signal.attrib.get("NameBetriebsstelle", "?"), 'blue'),
             colored(signal.attrib.get("Signalname", "?"), 'blue', attrs=['bold']),
-
-            element.attrib.get("Nr", 0),
-            richtung,
-
+            rp,
             ("Zeile" if not ersatzsignal else (colored("Ersatzsignal", 'grey', attrs=['underline']) + 'zeile')),
             zeile,
             colored(str_geschw(hsig_geschw), 'red', attrs=['bold']),
         ))
 
         ksig = signal.find("./KoppelSignal")
-        (element_alt, richtung_alt) = (None, None)
         indent = 2
         while ksig is not None:
-            datei = ksig.find("./Datei")
-            if datei is not None:
-                dateiname = datei.attrib.get("Dateiname", "")
-                if dateiname != "" and dateiname.upper() != str_dateiname:
-                    print(colored("   - Koppelsignal in Modul {}".format(dateiname), 'grey'))
-                    break
-            try:
-                (element, richtung) = referenzpunkte[int(ksig.attrib.get("ReferenzNr", 0))]
-            except KeyError:
-                print(colored("{} - Ungueltige Koppelsignal-Referenz".format(" " * indent), 'red', attrs=['bold']))
-                break
-            if (element, richtung) == (element_alt, richtung_alt):
-                print(colored("{} - Zirkelbezug in Koppelsignal".format(" " * indent)), 'red', attrs=['bold'])
-                break
-            koppelsignal = element.find("./Info" + richtung + "Richtung/Signal")
-            print("{} - Koppelsignal {} {} an Element {} {} auf Zeile {} ({})".format(
+            rp = get_refpunkt(get_modul_aus_dateiknoten(ksig), int(ksig.attrib.get("ReferenzNr", 0)))
+            koppelsignal = rp.element.find("./Info" + rp.richtung + "Richtung/Signal")
+            print("{} - Koppelsignal {} {} an {} auf Zeile {} ({})".format(
                 " " * indent,
                 colored(koppelsignal.attrib.get("NameBetriebsstelle", "?"), 'blue'),
                 colored(koppelsignal.attrib.get("Signalname", "?"), 'blue', attrs=['bold']),
-
-                element.attrib.get("Nr", 0),
-                richtung,
-
+                rp,
                 zeile,
                 colored(str_geschw(float(koppelsignal.findall("./HsigBegriff")[zeile].attrib.get("HsigGeschw", 0.0))), 'red', attrs=['bold']),
             ))
@@ -110,23 +127,14 @@ for f in tree.findall("./Strecke/Fahrstrasse"):
             ksig = koppelsignal.find("./KoppelSignal")
 
     for sig in f.findall("./FahrstrVSignal"):
-        datei = sig.find("./Datei")
-        if datei is not None:
-            dateiname = datei.attrib.get("Dateiname", "")
-            if dateiname != "" and dateiname.upper() != str_dateiname:
-                print(colored(" - Vorsignal in Modul {}".format(dateiname), 'grey'))
-                continue
-        (element, richtung) = referenzpunkte[int(sig.attrib.get("Ref", 0))]
-        signal = element.find("./Info" + richtung + "Richtung/Signal")
+        rp = get_refpunkt(get_modul_aus_dateiknoten(sig), int(sig.attrib.get("Ref", 0)))
+        signal = rp.element.find("./Info" + rp.richtung + "Richtung/Signal")
         spalte = int(sig.attrib.get("FahrstrSignalSpalte", 0))
         vsig_geschw = float(signal.findall("./VsigBegriff")[spalte].attrib.get("VsigGeschw", 0.0))
-        print(" - Vorsignal {} {} an Element {} {} auf Spalte {} ({}) {}".format(
+        print(" - Vorsignal {} {} an {} auf Spalte {} ({}) {}".format(
             colored(signal.attrib.get("NameBetriebsstelle", "?"), 'cyan'),
             colored(signal.attrib.get("Signalname", "?"), 'cyan', attrs=['bold']),
-
-            element.attrib.get("Nr", 0),
-            richtung,
-
+            rp,
             spalte,
             colored(str_geschw(vsig_geschw), 'green', attrs=['bold']),
             colored("!!!!", 'red', attrs=['bold']) if geschw_kleiner(min_geschw, vsig_geschw) else "",
